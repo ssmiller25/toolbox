6a9576b083f8d28bc58c70e0f766237b7f84b6bc
Steve Miller <me@r15cookie.com>
2022-07-31 16:57:37 +0000

    eks toolbox, inital backup tests

---
+++ infra/stacks/eks/README.md
@@ -0,0 +1,10 @@
+# Disk benchmark work
+
+Basic EKS cluster
+
+To login to eks cluster after apply
+
+```sh
+aws eks update-kubeconfig --name toolbox-eks
+```
+
---
+++ infra/stacks/eks/versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    civo = {
+      source = "civo/civo"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-r15cookie"
+    key    = "stacks/eks"
+    dynamodb_table = "terraform-r15cookie"
+    region = "us-east-2"
+  }
+
+}
+
+provider "aws" {
+
+}
+
+provider "civo" {
+  region = "NYC1"
+}
